6603b77acbdb20e0e97112398debdd4629136bb3
Akiragon77 <120325792+Akiragon77@users.noreply.github.com>
2022-12-12 02:13:12 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+import time
+
+st.title('Streamlit 超入門')
+
+st.write('プレグレスバー')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration{i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)
+
+'Done!!!'
+
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラム')
+
+expander = st.expander('問い合わせ1')
+expander.write('問い合わせ内容を書く')
+expander = st.expander('問い合わせ2')
+expander.write('問い合わせ内容を書く')
+expander = st.expander('問い合わせ3')
+expander.write('問い合わせ内容を書く')
+
+
+
+
+
+# text = st..text_input('あなたの趣味を教えて下さい。')
+# condition = st..slider('あなたの今の調子は？', 0 ,100 ,50)
+
+# 'あなたの趣味：', text, 'です。'
+# 'コンディション:', condition
+
+
+
+#'あなたの好きな数字は、', text, 'です。'
+
+
+#if st.checkbox('Show Image'):
+#  img = Image.open('sample.jpg')
+#  st.image(img, caption='Dog', use_column_width=True)
+
+
+
